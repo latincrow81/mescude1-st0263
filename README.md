d25131eedb249d8fe20af8be8a7b464bc7f024dc
Mauricio Escudero <mauricio.escudero@patientpop.com>
2023-03-06 23:07:14 -0500

    wip grpc api gtw

--- README.md
+++ README.md
@@ -1,26 +1,29 @@
 # ST0263 Topicos Especiales En Telematica
 #
-# Estudiante(s): Mauricio Escudero Restrepo , mescude1@eafit.edu.co
+## Estudiante(s): Mauricio Escudero Restrepo , mescude1@eafit.edu.co
 #
-# Profesor: Edwin Nelson Montoya Munera, emontoya@eafit.edu.co
+## Profesor: Edwin Nelson Montoya Munera, emontoya@eafit.edu.co
 #
-# Reto No 2. Procesos comunicantes por API REST, RPC y MOM
+### Reto No 2. Procesos comunicantes por API REST, RPC y MOM
 #
  Diseño e implementación de 2 microservicios básicos que ofrecen ambos un servicio al API Gateway y que se deben 
  comunicar por un middleware RPC y por un middleware MOM.
 #
 
-# Como ejecutar
+#### Como ejecutar
 
 Este proyecto requiere Docker para funcionar.
 
 <code>docker-compose up</code>
 
+El Api Gateway funciona en el puerto 5000 y provee una interfaz tipo swagger en localhost:5000/ui
 
-# referencias
-## https://x-team.com/blog/set-up-rabbitmq-with-docker-compose/
-## https://blog.logrocket.com/creating-a-web-server-with-golang/
-## https://github.com/sahansera/go-grpc
-## https://github.com/lakhinsu/rabbitmq-go-example
 
-#### versión README.md -> 1.0 (2023-Marzo)
+#### referencias
+* https://x-team.com/blog/set-up-rabbitmq-with-docker-compose/
+* https://blog.logrocket.com/creating-a-web-server-with-golang/
+* https://github.com/sahansera/go-grpc
+* https://github.com/lakhinsu/rabbitmq-go-example
+* https://medium.com/@rahulsamant_2674/python-rabbitmq-8c1c3b79ab3d
+
+##### versión README.md -> 1.0 (2023-Marzo)
--- api_gateway/app/services/rmq_subscriber.py
+++ api_gateway/app/services/rmq_subscriber.py
@@ -1,8 +1,42 @@
+import pika
+
+
 class RmqSubscriberService:
 
     def __init__(self):
         self.config = {
             'host': 'localhost',
             'port': 5672,
-            'exchange': 'topic'
+            'exchange': 'gateway',
+            'queueName': 'Response',
+            'bindingKey': 'response'
         }
+
+    def _create_connection(self):
+        parameters = pika.ConnectionParameters(host=self.config['host'],
+                                               port=self.config['port'])
+        return pika.BlockingConnection(parameters)
+
+    def on_message_callback(self, channel, method, properties, body):
+        binding_key = method.routing_key
+        print('received new message for -' + binding_key)
+
+    def listen_for_rmq_response(self):
+        connection = self._create_connection()
+        channel = connection.channel()
+        channel.exchange_declare(exchange=self.config['exchange'],
+                                 exchange_type='topic')
+
+        channel.queue_declare(queue=self.config['queueName'])
+
+        channel.queue_bind(queue=self.config['queueName'],
+                           exchange=self.config['exchange'],
+                           routing_key=self.config['bindingKey'])
+
+        channel.basic_consume(queue=self.config['queueName'],
+                              on_message_callback=self.on_message_callback, auto_ack=True)
+        print('[*]  Waiting for data for ' + self.config['queueName'] + '.To exit press CTRL+C')
+        try:
+            channel.start_consuming()
+        except KeyboardInterrupt:
+            channel.stop_consuming()
