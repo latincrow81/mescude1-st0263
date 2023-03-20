42c3febe417156a95d927a38747f3c70dcc3e62c
Mauricio Escudero <mauricio.escudero@patientpop.com>
2023-03-20 16:30:52 -0500

    reto 3 initial

--- api_gateway/app/controllers.py
+++
@@ -1,9 +0,0 @@
-from flask import Response
-
-
-def get_file_list():
-    return Response(status=200, response={})
-
-
-def find_file():
-    return Response(status=404, response={"error": "Archivo no encontrado"})
--- api_gateway/docker-entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-set -e
-
-gunicorn --enable-stdio-inheritance --bind 0.0.0.0:5000 --reload wsgi:app
